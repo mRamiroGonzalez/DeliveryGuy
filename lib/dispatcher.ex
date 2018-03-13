2fa600d5d41bed4609e7fea5a043d9166e7f132f
Maxime Ramiro-Gonzalez <maxime.ramiro@hotmail.com>
2018-03-13 21:43:42 +0100

    dispatch requests to chain sync / async

--- lib/dispatcher.ex
+++ lib/dispatcher.ex
@@ -1,20 +1,33 @@
 defmodule Dispatcher do
 
+  def process_routes(routes) do
+    routesMap = routes |> File.read! |> Poison.decode!
+    codes = Enum.reduce routesMap, [], fn ({_id, route}, acc) ->
+      codes = cond do
+        Map.has_key?(route, "async") ->
+          deliver_route_async(route)
+        Map.has_key?(route, "sync") ->
+          deliver_route(route)
+      end
+      [acc | codes]
+    end
+    List.flatten(codes)
+  end
+
   def deliver_route(route) do
-    routeMap = Poison.decode! File.read! route
     {:ok, pid} = GenServer.start_link(Deliveryguy, [])
 
-    Enum.reduce routeMap["sync"], [], fn (houseInfos, acc) ->
+    codes = Enum.reduce route["sync"], [], fn (houseInfos, acc) ->
       code = Deliveryguy.deliver_house(pid, houseInfos)
       [code | acc]
     end
+    Enum.reverse(codes)
   end
 
   def deliver_route_async(route) do
-    routeMap = Poison.decode! File.read! route
     {:ok, pid} = GenServer.start_link(Deliveryguy, [])
 
-    routeMap["async"]
+    route["async"]
     |> Enum.map(&Task.async(fn -> Deliveryguy.deliver_house(pid, &1) end))
     |> Enum.map(&Task.await/1)
   end
--- test/dispatcher_test.exs
+++ test/dispatcher_test.exs
@@ -1,18 +1,29 @@
 defmodule DispatcherTest do
   use ExUnit.Case
 
-  test "request_post_multiple" do
-    filename = "test/routes/create-two-events.json"
-    codes = Dispatcher.deliver_route(filename)
+  test "request_multiple" do
+    filename = "test/routes/create-and-get-event.json"
+    routesMap = filename |> File.read! |> Poison.decode!
 
-    assert codes == [201, 201]
+    codes = Dispatcher.deliver_route(routesMap)
+
+    assert codes == [201, 200]
   end
 
   test "request_post_multiple_async" do
     filename = "test/routes/create-two-events-async.json"
-    codes = Dispatcher.deliver_route_async(filename)
+    routesMap = filename |> File.read! |> Poison.decode!
+
+    codes = Dispatcher.deliver_route_async(routesMap)
 
     assert codes == [201, 201]
+  end
+
+  test "request_chain" do
+    filename = "test/routes/multiple-steps-requests.json"
+    codes = Dispatcher.process_routes(filename)
+
+    assert codes == [201, 201, 200, 201, 201, 201, 201]
   end
 
 end
