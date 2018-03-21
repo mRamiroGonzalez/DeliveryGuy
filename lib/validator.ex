e32349b36a3fc78adccf6b1919f7c08921211bd3
Maxime Ramiro-Gonzalez <maxime.ramiro@hotmail.com>
2018-03-21 21:18:19 +0100

    replace variable with pipe

--- lib/validator.ex
+++ lib/validator.ex
@@ -6,15 +6,14 @@ defmodule Validator do
   end
 
   def print_feedback(requestInfos, response, valid) do
-    output =
-      "#{requestInfos["request"]["method"]} #{requestInfos["request"]["to"]}"
-      <> "\n ╚ Expected: #{requestInfos["response"]["expect"]}"
-      <> if (not valid) do
-        " - FAILURE \n    ╚ Response code: #{response.status_code}\n    ╚ Response body: #{inspect response.body}"
-      else
-        " ✔"
-      end
-    IO.puts output
+    "#{requestInfos["request"]["method"]} #{requestInfos["request"]["to"]}"
+    <> "\n ╚ Expected: #{requestInfos["response"]["expect"]}"
+    <> if (not valid) do
+      " - FAILURE \n    ╚ Response code: #{response.status_code}\n    ╚ Response body: #{inspect response.body}"
+    else
+      " ✔"
+    end
+    |> IO.puts
     valid
   end
 end
